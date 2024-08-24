27e77fcb65b72adf8b31a873e5ee22e7f463c041
Pavithra8805 <pavithra8805@gmail.com>
2024-08-24 22:49:50 +0000

    Week 2 Day 5

---
+++ Week 2/Day5/Problem5.java
@@ -0,0 +1,68 @@
+// Balanced Binary Tree
+
+// Given a binary tree, determine if it is height-balanced
+
+
+// Example 1:
+// Input: root = [3,9,20,null,null,15,7]
+// Output: true
+
+// Example 2:
+// Input: root = [1,2,2,3,3,null,null,4,4]
+// Output: false
+
+class Solution {
+    
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        return checkHeight(root) != -1;
+    }
+
+    private int checkHeight(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftHeight = checkHeight(node.left);
+        if (leftHeight == -1) {
+            return -1;
+        }
+
+        int rightHeight = checkHeight(node.right);
+        if (rightHeight == -1) {
+            return -1;
+        }
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20, new TreeNode(15), new TreeNode(7));
+
+        Solution solution = new Solution();
+        System.out.println(solution.isBalanced(root));
+    }
+}
