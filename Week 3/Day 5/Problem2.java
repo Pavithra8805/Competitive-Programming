0f043144534ac3c9507e798d69e254cadac1bb75
Pavithra8805 <pavithra8805@gmail.com>
2024-09-03 10:56:49 +0000

    week 3 Day 5

---
+++ Week 3/Day 5/Problem2.java
@@ -0,0 +1,46 @@
+// Merge Intervals
+
+// Given an array of intervals where intervals[i] = [starti, endi], merge all
+// overlapping intervals, and return an array of the non-overlapping intervals
+// that cover all the intervals in the input.
+
+// Example 1:
+
+// Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+// Output: [[1,6],[8,10],[15,18]]
+// Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+// Example 2:
+
+// Input: intervals = [[1,4],[4,5]]
+// Output: [[1,5]]
+// Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+
+// Constraints:
+
+// 1 <= intervals.length <= 104
+
+
+import java.util.*;
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length <= 1) return intervals;
+        
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        List<int[]> result = new ArrayList<>();
+        
+        int[] currentInterval = intervals[0];
+        result.add(currentInterval);
+        
+        for (int[] interval : intervals) {
+            if (interval[0] <= currentInterval[1]) {
+                currentInterval[1] = Math.max(currentInterval[1], interval[1]);
+            } else {
+                currentInterval = interval;
+                result.add(currentInterval);
+            }
+        }
+        
+        return result.toArray(new int[result.size()][]);
+    }
+}
